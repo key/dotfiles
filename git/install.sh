bf9e5759decbb13d20cd7ea4294f843e2811cfe9
key <mitsukuni.sato@gmail.com>
2021-06-29 10:24:34 +0900

    set gitignore

---
+++ git/install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+mkdir -p ~/.config/git
+
+TMPFILE=$(mktemp)
+echo "$TMPFILE"
+
+# tools
+{
+	cat ../vendor/github/gitignore/Global/JetBrains.gitignore
+	cat ../vendor/github/gitignore/Global/Xcode.gitignore
+	cat ../vendor/github/gitignore/Global/macOS.gitignore
+	cat ../vendor/github/gitignore/Global/Ninja.gitignore
+} >>"$TMPFILE"
+
+# programming language
+{
+	cat ../vendor/github/gitignore/Python.gitignore
+	cat ../vendor/github/gitignore/C.gitignore
+	cat ../vendor/github/gitignore/C++.gitignore
+	cat ../vendor/github/gitignore/CMake.gitignore
+	cat ../vendor/github/gitignore/Go.gitignore
+	cat ../vendor/github/gitignore/Ruby.gitignore
+	cat ../vendor/github/gitignore/Rust.gitignore
+	cat ../vendor/github/gitignore/Sass.gitignore
+	cat ../vendor/github/gitignore/Terraform.gitignore
+	cat ../vendor/github/gitignore/Objective-C.gitignore
+	cat ../vendor/github/gitignore/Swift.gitignore
+} >>"$TMPFILE"
+
+mv "$TMPFILE" ~/.config/git/ignore
+
+echo "Succeeded!"
